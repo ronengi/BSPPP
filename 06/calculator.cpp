985c44a05a09aec168d28eb9975431875b5fb483
Ronen Gilead-Raz <ronengi@gmail.com>
2017-11-17 18:38:52 +0200

    MOD first basic working calculator

--- 06/calculator.cpp
+++ 06/calculator.cpp
@@ -2,8 +2,17 @@
 
 
 
-Token get_token() {
-}
+class Token {
+public:
+    char kind;
+    double value;
+    Token(char ch)
+        :kind(ch), value(0) { }
+    Token(char ch, double val)
+        :kind(ch), value(val) { }
+};
+
+//----------------------------------------------------------------------------------
 
 class Token_stream {
 public:
@@ -15,13 +24,96 @@ private:
     Token buffer;
 };
 
+//----------------------------------------------------------------------------------
+
+Token_stream::Token_stream()
+    :full(false), buffer(0) { }
+
 void Token_stream::putback(Token t) {
+    if (full)
+        error ("putback() into a full buffer");
     buffer = t;
     full = true;
 }
 
+//----------------------------------------------------------------------------------
 
-////////////////////////////////////////////////////////////////
+Token Token_stream::get() {
+    if (full) {
+        full = false;
+        return buffer;
+    }
+    char ch;
+    cin >> ch;
+    switch(ch) {
+    case '=':
+    case 'q':
+    case '(': case ')': case '+': case '-': case '*': case '/':
+        return Token{ch};
+    case '.':
+    case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': {
+        cin.putback(ch);
+        double val;
+        cin >> val;
+        return Token{'8', val};
+    }
+    default:
+        error ("bad token");
+    }
+}
+
+//----------------------------------------------------------------------------------
+
+Token_stream ts;
+
+//----------------------------------------------------------------------------------
+
+double expression();
+
+//----------------------------------------------------------------------------------
+
+double primary() {
+    Token t = ts.get();
+    switch (t.kind) {
+    case '(': {
+        double d = expression();
+        t = ts.get();
+        if (t.kind != ')')    error("')' expected");
+        return d;
+    }
+    case '8':       // represents a number
+        return t.value;
+    default:
+        error ("primary expected");
+    }
+}
+
+//----------------------------------------------------------------------------------
+
+double term() {
+    double left = primary();
+    Token t = ts.get();
+    while (true) {
+        switch (t.kind) {
+        case '*':
+            left *= primary();
+            t = ts.get();
+            break;
+        case '/': {
+            double d = primary();
+            if (d == 0)    error("division by zero");
+            left /= d;
+            t = ts.get();
+            break;
+        }
+        default:
+            ts.putback(t);
+            return left;
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------
 
 double expression() {
     double left = term();
@@ -37,67 +129,34 @@ double expression() {
             t = ts.get();
             break;
             default:
-                ts.putback();
+                ts.putback(t);
                 return left;
         }
     }
 }
 
-double term() {
-    double left = primary();
-    Token t = ts.get();
-    while (true) {
-        switch (t.kind) {
-        case '*':
-            left *= primary();
-            t = ts.get();
-            break;
-        case '/': {
-            double d = primary();
-            if (d == 0)    error("division by zero");
-            lefe /= d;
-            t = ts.get();
-            break;
-        }
-        default:
-            ts.putback();
-            return left;
-        }
-    }
-}
-
-double primary() {
-    Token t = ts.get();
-    switch (t.kind) {
-    case '(': {
-        double d = expression();
-        t = ts.get();
-        if (t.kind != ')')    error("')' expected");
-        return d;
-    }
-    case '8':       // represents a number
-        retuen t.value;
-    default:
-        error ("primary expected");
-    }
-}
-
-
+//----------------------------------------------------------------------------------
 
 int main() {
+    double val = 0.0;
     try {
+        cout << "Expression: ";
         while (cin) {
             Token t = ts.get();
             if (t.kind == 'q')
                 break;
-            if (t.kind == ';')
+            if (t.kind == '=') {
                 cout << "=" << val << "\n";
-            else
+                cout << "Expression: ";
+            }
+            else {
                 ts.putback(t);
-            val = expression();
+                val = expression();
+            }
+        }
     }
     catch (exception& e) {
-        cerr << e.what() << "/n";
+        cerr << e.what() << "\n";
         return 1;
     }
     catch (...) {
