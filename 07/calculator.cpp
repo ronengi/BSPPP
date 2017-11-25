85f542676c44b92179e2e12cad18220ab96d4190
Ronen Gilead-Raz <ronengi@gmail.com>
2017-11-25 22:24:40 +0200

    MOD finish grammar and calculator as in chapter 07

--- 07/calculator.cpp
+++ 07/calculator.cpp
@@ -3,27 +3,96 @@
 
 const char number = '8';
 const char quit = 'q';
-const char print = '=';
+const char print = ';';
+const char name = 'a';
+const char let = 'L';
+const char assign = '=';
 const string prompt = "> ";
 const string result = "= ";
+const string declkey = "let";
 
 
+/*******************************************************************************
+ ******************************************************************************/
+class Variable {
+public:
+    string name;
+    double value;
+};
+
+/*******************************************************************************
+ ******************************************************************************/
+vector<Variable> var_table;
+
+/*******************************************************************************
+ * Return the value of a variable named s
+ ******************************************************************************/
+double get_value(string s) {
+    for (const Variable& v : var_table) {
+        if (v.name == s)
+            return v.value;
+    }
+    stringstream err;
+    err << "get: undefined variable: '" << s << "'";
+    error(err.str());
+    return 0.0;
+}
+
+
+
+/*******************************************************************************
+ * Set the value named s to d
+ ******************************************************************************/
+void set_value(string s, double d) {
+    for (Variable& v : var_table) {
+        if (v.name == s) {
+            v.value = d;
+            return;
+        }
+    }
+    stringstream err;
+    err << "set: undefined variable: '" << s << "'";
+    error(err.str());
+}
+
+/*******************************************************************************
+ * Is var already in var_table ?
+ ******************************************************************************/
+bool is_declared(string var) {
+    for (const Variable& v : var_table) {
+        if (v.name == var)
+            return true;
+    }
+    return false;
+}
+
+/*******************************************************************************
+ ******************************************************************************/
+/* Add {var, val} to var_table */
+double define_name(string var, double val) {
+    if (is_declared(var)) {
+    stringstream err;
+    err << "'" << var << "' declared twice";
+        error(err.str());
+    }
+    var_table.push_back(Variable{var, val});
+    return val;
+}
+
+/*******************************************************************************
+ ******************************************************************************/
 class Token {
 public:
     char kind;
     double value;
-
-    Token(char ch)
-    : kind(ch), value(0) {
-    }
-
-    Token(char ch, double val)
-    : kind(ch), value(val) {
-    }
+    string name;
+    Token(char ch) :kind(ch), value(0) { }                  // initialize kind
+    Token(char ch, double val) :kind(ch), value(val) { }    // initialize kind and value
+    Token(char ch, string n) :kind(ch), name(n) { }         // initialize kind and name
 };
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 class Token_stream {
 public:
     Token_stream();
@@ -35,14 +104,14 @@ private:
     Token buffer;
 };
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 Token_stream::Token_stream()
 : full(false), buffer(0) {
 }
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 void Token_stream::putback(Token t) {
     if (full)
         error("putback() into a full buffer");
@@ -50,8 +119,8 @@ void Token_stream::putback(Token t) {
     full = true;
 }
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 Token Token_stream::get() {
     if (full) {
         full = false;
@@ -62,6 +131,7 @@ Token Token_stream::get() {
     switch (ch) {
         case quit:
         case print:
+        case assign:
         case '(':
         case ')':
         case '+':
@@ -81,26 +151,49 @@ Token Token_stream::get() {
             return Token{number, val};
         }
         default:
-            error("bad token");
+            char ch1 = ch;
+            if (isalpha(ch)) {      // a letter followed by a sequence of letters and digits
+                string s;
+                s += ch;
+                while (cin.get(ch) && (isalpha(ch) || isdigit(ch)))
+                    s += ch;
+                cin.putback(ch);
+                if (s == declkey)       // declaration keyword
+                    return Token(let);
+                return Token{name, s};
+            }
+            stringstream err;
+            err << "bad token: '" << ch1 << "'";
+            error(err.str());
     }
 }
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 void Token_stream::ignore(char c) {
-    // TODO: continue here from p.273
+    if (full && c == buffer.kind) {
+        full = false;
+        return;
+    }
+    full = false;
+
+    char ch = 0;
+    while (cin >> ch) {
+        if (c == ch)
+            return;
+    }
 }
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 Token_stream ts;
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 double expression();
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 double primary() {
     Token t = ts.get();
     switch (t.kind) {
@@ -113,6 +206,8 @@ double primary() {
         }
         case number: // represents a number
             return t.value;
+        case name:
+            return get_value(t.name);
         case '-':
             return - primary();
         case '+':
@@ -122,8 +217,8 @@ double primary() {
     }
 }
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 double factorial() {
     double left = primary();
     Token t = ts.get();
@@ -147,8 +242,8 @@ double factorial() {
     }
 }
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 double term() {
     double left = factorial();
     Token t = ts.get();
@@ -181,8 +276,8 @@ double term() {
     }
 }
 
-//----------------------------------------------------------------------------------
-
+/*******************************************************************************
+ ******************************************************************************/
 double expression() {
     double left = term();
     Token t = ts.get();
@@ -203,23 +298,65 @@ double expression() {
     }
 }
 
-//----------------------------------------------------------------------------------
+/*******************************************************************************
+ ******************************************************************************/
+double declaration() {
+    // assume we have seen "let"
+    // handle: name = expression
+    // declare a variable called "name" with the initial value "expression"
+    Token t = ts.get();
+    if (t.kind != name)
+        error("name expected in declaration");
+    string var_name = t.name;
+    Token t2 = ts.get();
+    if (t2.kind != assign) {
+        stringstream err;
+        err << "'" << assign << "' missing in declaration of: '" << var_name << "'";
+        error(err.str());
+    }
 
+    double d = expression();
+    define_name(var_name, d);
+    return d;
+}
+
+/*******************************************************************************
+ ******************************************************************************/
+double statement() {
+    Token t = ts.get();
+    switch (t.kind) {
+        case let:
+            return declaration();
+        default:
+            ts.putback(t);
+            return expression();
+    }
+}
+
+/*******************************************************************************
+ ******************************************************************************/
 void calculate() {
-        while (cin) {
+    while (cin) {
+        try {
             cout << prompt;
             Token t = ts.get();
             while (t.kind == print)
-                t = ts.get();
+                t = ts.get();       // consume all "prints"
             if (t.kind == quit)
-                return 0;
+                return;             // quit
             ts.putback(t);
-            cout << result << expression() << "\n";
+            cout << result << statement() << "\n";
         }
+        catch (exception& e) {
+            cerr << "exception: " << e.what() << "\n";
+            ts.ignore(print);       // clean up mess
+        }
+    }
 }
 
-//----------------------------------------------------------------------------------
 
+/*******************************************************************************
+ ******************************************************************************/
 int main() {
     try {
         calculate();
