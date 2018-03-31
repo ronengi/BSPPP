ecfb73b2de4ae2df29903c8fbb4dbc1df3f4863e
Ronen Gilead-Raz <ronengi@gmail.com>
2018-03-31 17:27:21 +0300

    Complete ch. 17

--- 17/Exercises_17/ex_09.cpp
+++ 17/Exercises_17/ex_09.cpp
@@ -4,7 +4,32 @@
  * and open the template in the editor.
  */
 
+#include <iostream>
+
+using namespace std;
+
 
 void main_09() {
 
+    int a = 1;
+    int b = 2;
+
+    int* pa = &a;
+    int *pb = &b;
+
+    cout << "pa== " << pa << "\n";
+    cout << "pb== " << pb << "\n";
+
+    cout << "\n";
+
+    int* pc = new int[40];
+    int* pd = new int;
+
+    cout << "pc== " << pc << "\n";
+    cout << "pd== " << pd << "\n";
+
+    cout << "\n";
+
+    delete[] pc;
+    delete pd;
 }
