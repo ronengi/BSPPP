0bd7971d8f0c92ea62798128ed04b026ba404042
Ronen Gilead-Raz <ronengi@gmail.com>
2018-05-05 19:06:05 +0300

    ADD ch 21, and first try-it

---
+++ 21/Tries_21/Record.hpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   Record.hpp
+ * Author: Ronen Gilead-Raz <ronengi@gmail.com>
+ *
+ * Created on May 5, 2018, 1:37 PM
+ */
+
+#ifndef RECORD_HPP
+#define RECORD_HPP
+
+struct Record {
+    double unit_price;
+    unsigned int units;          // number of units sold
+};
+
+#endif /* RECORD_HPP */
+
