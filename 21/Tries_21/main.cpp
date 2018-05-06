495163938affda1fd706576504e0a03cdfbac46a
Ronen Gilead-Raz <ronengi@gmail.com>
2018-05-06 08:07:39 +0300

    MOD re arrange ch 21 tries

--- 21/Tries_21/main.cpp
+++ 21/Tries_21/main.cpp
@@ -12,65 +12,16 @@
  */
 
 #include <iostream>
-#include <algorithm>
-#include <numeric>
-#include <vector>
-#include "Record.hpp"
 using namespace std;
 
-
-
-struct total_price {
-    double operator()(double val, const Record& rec) {
-        return val + rec.unit_price * rec.units;
-    }
-};
-
-
-struct unit_price {
-    double operator()(double val, const Record& rec) {
-        return val + rec.unit_price;
-    }
-};
-
-
-struct total_units {
-    unsigned int operator()(unsigned int val, const Record& rec) {
-        return val + rec.units;
-    }
-};
-
+int prices();
 
 /*
  *
  */
 int main(int argc, char** argv) {
 
-    int n = 2;
-    vector<Record> recs;
-    for(int i = 0; i < n; ++i) {
-        Record newrec;
-        newrec.unit_price = 2.0;
-        newrec.units = 1;
-        // recs[i].unit_price = i+1;
-        // recs[i].units = 10-i;
-        recs.push_back(newrec);
-    }
-
-    double total = 0.0;
-    total = accumulate(recs.begin(), recs.end(), total, total_price());
-    cout << "total price: " << total << "\n";
-
-
-    unsigned int total1 = 0;
-    total1 = accumulate(recs.begin(), recs.end(), total1, total_units());
-    cout << "total units: " << total1 << "\n";
-
-
-    total = 0.0;
-    total = accumulate(recs.begin(), recs.end(), total, unit_price());
-    cout << "total units price: " << total << "\n";
-
+    // prices();
 
     return 0;
 }
